68f3a4c8e6b83482d469d41ec38f754b06a9d019
sdmueller <simon.mueller@simondo.ch>
2015-12-06 17:38:44 +0100

    added .gitignore

---
+++ src/main/java/colltask/CollTaskProcessApplication.java
@@ -0,0 +1,8 @@
+package colltask;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+public class CollTaskProcessApplication extends ServletProcessApplication {
+
+}
