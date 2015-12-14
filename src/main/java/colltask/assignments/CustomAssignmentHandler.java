463ffaaed9b00f896cf1a22d9eec57e282144ab2
sdmueller <simon.mueller@simondo.ch>
2015-12-14 01:09:36 +0100

    added  due dates, postponement/rejection of postponement

--- src/main/java/colltask/assignments/CustomAssignmentHandler.java
+++ src/main/java/colltask/assignments/CustomAssignmentHandler.java
@@ -1,22 +1,34 @@
 package colltask.assignments;
 
+import java.util.Date;
+
 import org.camunda.bpm.engine.delegate.DelegateTask;
 import org.camunda.bpm.engine.delegate.TaskListener;
 
 public class CustomAssignmentHandler implements TaskListener {
 	
-	private static String chosenUser = "";
+//	private static String chosenUser = "";
 
 	public void notify(DelegateTask delegateTask) {
 		
-		String taskId = delegateTask.getId();
+//		String taskId = delegateTask.getId();
+//		
+//		if(taskId.equals("choosePerson")) {
+//			chosenUser = (String)delegateTask.getVariable("choosePersonFromList");
+//		} else if(taskId.equals("workOnTask")) {
+//			delegateTask.setAssignee(chosenUser);
+//		} else {
+//			//well fuck
+//		}
 		
-		if(taskId.equals("choosePerson")) {
-			chosenUser = (String)delegateTask.getVariable("choosePersonFromList");
-		} else if(taskId.equals("workOnTask")) {
-			delegateTask.setAssignee(chosenUser);
-		} else {
-			//well fuck
+		//Postponement handling ----------------------------------------
+		boolean allowPostponement = (Boolean) delegateTask.getVariable("allowPostponement");
+		
+		if(allowPostponement) {
+			delegateTask.setVariable("deadline", (Date) delegateTask.getVariable("postponeDate"));
 		}
+		delegateTask.setDueDate((Date) delegateTask.getVariable("deadline"));
+		
+		
 	}
 }
